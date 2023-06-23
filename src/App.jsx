14558374c7f2a77b1fb26f84907eba90d43fe1ff
Hoàng Văn Hiệu <104782203+hoanghieu2001@users.noreply.github.com>
2023-06-23 18:38:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+
+import './App.css'
+import './data.jsx'
+
+
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './data.jsx'
+import './index.css'
+import "./data.jsx"
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.js';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
